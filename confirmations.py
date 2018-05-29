028509e234320a57d3e283e04f0adf2b9191d2cd
tim <tim@manjaro.fritz.box>
2018-05-30 00:06:13 +0200

    initial commit

---
+++ confirmations.py
@@ -0,0 +1,42 @@
+##Calculate the number of confirmations necessary
+## To make a 51% attack uneconomical
+import math
+
+blocktimeMinutes = 2 # in minutes
+attackCostHour = 21000 #$
+productCost = 500000 #$
+margin = 1.2 # add 20%
+baselineConfirmations = 6
+print("Set the parameters for your currency, leave empty to choose the defaults")
+i = input("\nBlocktime in Minutes(default="+str(blocktimeMinutes)+"):")
+if i != "":
+	blocktimeMinutes = float(i)
+i = input("\nAttack cost per hour(default="+str(attackCostHour)+"):")
+if i != "":
+	attackCostHour = float(i)
+i = input("\nCost of your product(default="+str(productCost)+"):")
+if i != "":
+	productCost = float(i)
+i = input("\nmargin(default="+str(margin)+"):")
+if i != "":
+	margin = float(i)
+
+print("\nDue to normal fluctuations in the network, it is recommended to wait for a few confirmations regardsless of an attack.")
+i = input("Confirmation baseline(default="+str(baselineConfirmations)+"):")
+if i != "":
+	baselineConfirmations = float(i)
+
+print("\n\n")
+print("Note that the numbers provided by this program might not fit your business situation! If an attacker is able to buy multiple products at the same time for example, a attack might still be viable for them.")
+
+
+recommendedConfirmations = productCost/(attackCostHour*(blocktimeMinutes/60))*margin + baselineConfirmations
+
+print("Recommended number of Confirmations:", int(recommendedConfirmations))
+delay = recommendedConfirmations*(blocktimeMinutes/60)
+if delay > 1.1:
+	print("Delay: ", delay, "h")
+else:
+	print("Delay: ", delay*60, "min")
+
+
